437bd3fbe1bfa816c9af0e0d62e0e4d34e5a54a5
SagarREDDY742003 <sagar07042003@gmail.com>
2026-01-28 15:07:06 +0530

    first commit

---
+++ Python/DataTypes.py
@@ -0,0 +1,45 @@
+#Integers and floats - Represents whole numbers and decimal numbers.
+age=25
+height=5.9
+
+#Strings - Represents sequences of characters.
+name="John Doe"
+greeting="Hello, " + name + "!"
+
+#Booleans - Represents True or False values.
+is_student=True
+has_graduated=False
+
+#Lists - Represents ordered, mutable collections of items.
+fruits=["apple", "banana", "cherry"]
+numbers=[1, 2, 3, 4, 5]
+
+#tuples - Represents ordered, immutable collections of items.
+coordinates=(10.0, 20.0)
+colors=("red", "green", "blue")
+
+#Dictionaries - Represents key-value pairs.
+person={"name": "Alice", "age": 30, "city": "New York"}
+car={"make": "Toyota", "model": "Camry", "year": 2020}
+
+#Sets - Represents unordered collections of unique items.
+unique_numbers={1, 2, 3, 4, 5}
+unique_fruits={"apple", "banana", "cherry"}
+
+#NoneType - Represents the absence of a value.
+data=None
+
+#Complex Numbers - Represents numbers with real and imaginary parts.
+complex_number=3 + 4j
+
+#Byte and Bytearray - Represents sequences of bytes.
+byte_data=b"Hello, World!"
+byte_array=bytearray([65, 66, 67, 68, 69])
+
+#Range - Represents an immutable sequence of numbers.
+number_range=range(1, 11)
+
+#Frozen Set - Represents an immutable set of unique items.
+frozen_set_example=frozenset([1, 2, 3, 4, 5])
+
+
